8f4d7544598e6a6a26b3c4a6dab017507c6210eb
Maxim Wilman <mwilman@brockhaus-ag.de>
2020-03-26 21:04:50 +0100

    initial commit

---
+++ src/app.service.ts
@@ -0,0 +1,16 @@
+import { HttpService, Injectable } from '@nestjs/common';
+import { map, tap } from 'rxjs/operators';
+
+@Injectable()
+export class AppService {
+  constructor(private http: HttpService) {
+  }
+
+  getHeroes() {
+    return this.http
+      .get('https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=1&apikey=dc98babb5a914cce3258693779c3e1b5&hash=34d63b967e6935a3f8456bf1ca7dce48')
+      .pipe(
+        map(res => res.data.data.results)
+      );
+  }
+}
