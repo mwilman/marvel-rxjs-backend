ede84d09eec8aa0eb77771f10fac08b9523ae046
Maxim Wilman <mwilman@brockhaus-ag.de>
2020-03-27 11:55:40 +0100

    decreased limit

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,7 +7,7 @@ export class AppService {
   }
 
   getHeroes(name: string) {
-    let URL = 'https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=1&apikey=dc98babb5a914cce3258693779c3e1b5&hash=34d63b967e6935a3f8456bf1ca7dce48';
+    let URL = 'https://gateway.marvel.com:443/v1/public/characters?limit=20&ts=1&apikey=dc98babb5a914cce3258693779c3e1b5&hash=34d63b967e6935a3f8456bf1ca7dce48';
     if (name) {
       URL = `${URL}&nameStartsWith=${name}`
     }
