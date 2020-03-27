9ab94ca940199ffd10d5cffe8e0bf74d020458dd
Maxim Wilman <mwilman@brockhaus-ag.de>
2020-03-27 10:36:48 +0100

    added name param

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('api')
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('heroes')
-  gerHeroes() {
-    return this.appService.getHeroes();
+  gerHeroes(@Query("name") search: string) {
+    return this.appService.getHeroes(search);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -6,9 +6,14 @@ export class AppService {
   constructor(private http: HttpService) {
   }
 
-  getHeroes() {
+  getHeroes(name: string) {
+    let URL = 'https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=1&apikey=dc98babb5a914cce3258693779c3e1b5&hash=34d63b967e6935a3f8456bf1ca7dce48';
+    if (name) {
+      URL = `${URL}&nameStartsWith=${name}`
+    }
+    console.log(URL);
     return this.http
-      .get('https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=1&apikey=dc98babb5a914cce3258693779c3e1b5&hash=34d63b967e6935a3f8456bf1ca7dce48')
+      .get(URL)
       .pipe(
         map(res => res.data.data.results)
       );
