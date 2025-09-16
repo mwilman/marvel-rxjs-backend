3789866e86eb3c3816ed477a941409c315a7c1bc
Maxim Wilman <mwilman@brockhaus-ag.de>
2025-09-16 11:43:00 +0200

    updated to nest-cli 11

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
-import { HttpModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [HttpModule],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
-import { HttpService, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { map, tap } from 'rxjs/operators';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class AppService {
@@ -7,15 +8,16 @@ export class AppService {
   }
 
   getHeroes(name: string) {
-    let URL = 'https://gateway.marvel.com:443/v1/public/characters?limit=20&ts=1&apikey=dc98babb5a914cce3258693779c3e1b5&hash=34d63b967e6935a3f8456bf1ca7dce48';
+    let URL =
+      'https://gateway.marvel.com:443/v1/public/characters?limit=20&ts=1&apikey=dc98babb5a914cce3258693779c3e1b5&hash=34d63b967e6935a3f8456bf1ca7dce48';
     if (name) {
-      URL = `${URL}&nameStartsWith=${name}`
+      URL = `${URL}&nameStartsWith=${name}`;
     }
     console.log(URL);
-    return this.http
-      .get(URL)
+    return this.http.get(URL)
       .pipe(
-        map(res => res.data.data.results)
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        map((res) => res.data.data.results)
       );
   }
 }
